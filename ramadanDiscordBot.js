2ac7c0bab069ed39700848396013fd9d44cf96aa
Allal Mohamed Lamine <allalmohmaedlamine@gmail.com>
2018-06-02 21:31:11 +0100

    bot discord.js  command handling added

--- ramadanDiscordBot.js
+++ ramadanDiscordBot.js
@@ -1,12 +1,24 @@
 const Discord = require('discord.js');
+const fs = require('fs');
+
 const client = new Discord.Client();
+
+let config = require('./botConfig.json');
+
 
 client.on('ready', () => ***REMOVED***
   console.log(`Logged in as $***REMOVED***client.user.tag}!`);
 });
-const prefix = '!';
+
 client.on('message', msg => ***REMOVED***
-    const args = message.content.slice(prefix.length)
+    if(msg.content.substr(0, config.prefix.length) === config.prefix) ***REMOVED***
+        let args = msg.content.slice(config.prefix.length);
+        let command = args.shift().toLowerCase();
+
+        if(command === '') ***REMOVED***
+            
+      ***REMOVED***
+  ***REMOVED***
 });
 
 client.login('token');
